88fa4389ffd56acb4b3fb36bb90de0a934658b75
GreatCode1989 <wef666wef6@gmail.com>
2023-10-05 17:20:19 +0300

    lo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import { Center, Heading, Input, Button } from "@chakra-ui/react";
+import { Center, Heading, Input, List, ListItem } from "@chakra-ui/react";
 import LoginButton from "./LoginButton";
-import SearchButton from "./SearchButton"
+import SearchButton from "./SearchButton";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import axios from "axios"; 
+import axios from "axios";
 
 export default function Header() {
   const { data: session, status } = useSession();
@@ -38,33 +38,40 @@ export default function Header() {
 
   return (
     <Center h="100vh" flexDir="column">
-  <Heading as="h1" mb="4" fontSize="20px" mt="-150" pb="10">
-    {status === "authenticated" ? "Welcome" : "Login to get started"}
-  </Heading>
-  {status === "authenticated" && (
-    <>
-      <Input
-        placeholder="Enter GitHub username"
-        value={username}
-        onChange={handleUsernameChange}
-        mb="4"
-      />
-     
-      <SearchButton  onClick={fetchRepositories}/>
-    </>
-  )}
-  <div>
-    <ul>
-      {repositories.map((repo) => (
-        <li key={repo.id}>{repo.name}</li>
-      ))}
-    </ul>
-  </div>
-  {status !== "authenticated" && <LoginButton />}
-</Center>
-
+      <Heading as="h1" mb="4" fontSize="20px" mt="-150" pb="10">
+        {status === "authenticated" ? "Welcome" : "Login to get started"}
+      </Heading>
+      {status === "authenticated" && (
+        <>
+          <Input
+            placeholder="Enter GitHub username"
+            value={username}
+            onChange={handleUsernameChange}
+            mb="4"
+            width={200}
+            height={20}
+            p={4}
+          />
+          <SearchButton onClick={fetchRepositories} />
+        </>
+      )}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <List spacing={3} mt="22" p={0} style={{ overflowY: "auto", maxHeight: "300px" }}>
+          {repositories.map((repo) => (
+            <ListItem
+              key={repo.id}
+              p="5"
+              mt={13}
+              bg="lightblue"
+              borderRadius="12px"
+              textAlign="center"
+            >
+              {repo.name}
+            </ListItem>
+          ))}
+        </List>
+      </div>
+      {status !== "authenticated" && <LoginButton />}
+    </Center>
   );
 }
-
-
-
