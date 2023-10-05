628873b132b3964f5891ecb42014181e63f38d1c
GreatCode1989 <wef666wef6@gmail.com>
2023-10-05 14:50:12 +0300

    lo

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@
 
 import { Center, Heading, Input, Button } from "@chakra-ui/react";
 import LoginButton from "./LoginButton";
+import SearchButton from "./SearchButton"
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import axios from "axios"; 
@@ -37,29 +38,31 @@ export default function Header() {
 
   return (
     <Center h="100vh" flexDir="column">
-      <Heading as="h1" mb="4" fontSize="20px" mt="-150" pb="10">
-        {status === "authenticated" ? "Welcome" : "Login to get started"}
-      </Heading>
-      {status === "authenticated" && (
-        <>
-          <Input
-            placeholder="Enter GitHub username"
-            value={username}
-            onChange={handleUsernameChange}
-            mb="4"
-          />
-          <Button onClick={fetchRepositories}>Fetch Repositories</Button>
-        </>
-      )}
-      <div>
-        <ul>
-          {repositories.map((repo) => (
-            <li key={repo.id}>{repo.name}</li>
-          ))}
-        </ul>
-      </div>
-      {status !== "authenticated" && <LoginButton />}
-    </Center>
+  <Heading as="h1" mb="4" fontSize="20px" mt="-150" pb="10">
+    {status === "authenticated" ? "Welcome" : "Login to get started"}
+  </Heading>
+  {status === "authenticated" && (
+    <>
+      <Input
+        placeholder="Enter GitHub username"
+        value={username}
+        onChange={handleUsernameChange}
+        mb="4"
+      />
+     
+      <SearchButton  onClick={fetchRepositories}/>
+    </>
+  )}
+  <div>
+    <ul>
+      {repositories.map((repo) => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
+  </div>
+  {status !== "authenticated" && <LoginButton />}
+</Center>
+
   );
 }
 
---
+++ components/SearchButton.jsx
@@ -0,0 +1,32 @@
+
+import { Button } from "@chakra-ui/react";
+
+
+export default function SignInButton() {
+  return (
+    <Button
+      
+      colorScheme="green"
+      size="md"
+      borderRadius="8px"
+      fontWeight="bold"
+      boxShadow="md"
+      p={8}
+      mt={12}
+      width={200}
+      _hover={{
+        background: "linear-gradient(135deg, #ff0000, #007bff)",
+        cursor: "pointer",
+        color: "white",
+        transition: "background .2s",
+      }}
+      _active={{
+        background: "linear-gradient(135deg, #b3e0ff, #ff9900)",
+        transition: "background .2s",
+      }}
+      _focus={{ outline: "none" }}
+    >
+      Search Repositories
+    </Button>
+  );
+}
