4fe7a7fc73496acc0d9eeef837c75eeea1c9f5e7
GreatCode1989 <wef666wef6@gmail.com>
2023-10-05 12:50:23 +0300

    lo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,67 @@
-"use client";
+"use client"
 
-import { Center, Heading } from "@chakra-ui/react";
+import { Center, Heading, Input, Button } from "@chakra-ui/react";
 import LoginButton from "./LoginButton";
-import {useSession} from "next-auth/react"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import axios from "axios"; 
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const [repositories, setRepositories] = useState([]);
+  const [username, setUsername] = useState("");
 
-  if (status === "authenticated") {
-    return (
-      <Center h="100vh" flexDir="column">
-        <Heading as="h1" mb="4" fontSize="20px" mt="-150"pb="10">
-         Welcome
-        </Heading>
-        <span style={{ fontSize: "36px", color: "blue" }}>{session.user.name}</span>
-      </Center>
-    );}
-   
+  const fetchRepositories = () => {
+    if (status === "authenticated" && username) {
+      axios
+        .get(`https://api.github.com/users/${username}/repos`)
+        .then((response) => {
+          const repositoriesData = response.data;
+          setRepositories(repositoriesData);
+        })
+        .catch((error) => {
+          console.error("Error fetching GitHub repositories:", error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      fetchRepositories();
+    }
+  }, [status, username]);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
   return (
     <Center h="100vh" flexDir="column">
-      <Heading as="h1" mb="4" fontSize="20px" mt="-150"pb="10">
-       Login to get started
+      <Heading as="h1" mb="4" fontSize="20px" mt="-150" pb="10">
+        {status === "authenticated" ? "Welcome" : "Login to get started"}
       </Heading>
-      <LoginButton />
+      {status === "authenticated" && (
+        <>
+          <Input
+            placeholder="Enter GitHub username"
+            value={username}
+            onChange={handleUsernameChange}
+            mb="4"
+          />
+          <Button onClick={fetchRepositories}>Fetch Repositories</Button>
+        </>
+      )}
+      <div>
+        <ul>
+          {repositories.map((repo) => (
+            <li key={repo.id}>{repo.name}</li>
+          ))}
+        </ul>
+      </div>
+      {status !== "authenticated" && <LoginButton />}
     </Center>
   );
 }
+
+
+
